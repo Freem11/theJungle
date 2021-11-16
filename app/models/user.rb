3d559dab8759b1568abaaa43fb25ca4c06fedd15
Freem11 <freem1985@gmail.com>
2021-11-15 17:53:39 -0800

    user rspec done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,22 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
   
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 4 }
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,155 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+   
+    it "should generate a user if password and password confirmation match" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+   
+      expect(@user).to be_valid
+    end
+
+    it "should not generate a user if password and password confirmation dont match" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: "mayo"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if password and password confirmation are nil" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: nil,
+        password_confirmation: nil
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if password is nil" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: nil,
+        password_confirmation: "mayo"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if password confirmation is nil" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: nil
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if email is already in Db" do
+      @user1 = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "d@gmail.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+      @user1.save
+
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "D@gmail.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if email is nil" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: nil,
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+    it "should not generate a user if first name is nil" do
+      @user = User.new(
+        first_name: nil,
+        last_name: "Testington",
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+    it "should not generate a user if last name is nil" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: nil,
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+    it "should not generate a user if passowrd is too short (4)" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: nil,
+        email: "test@test.com",
+        password: "hey",
+        password_confirmation: "hey"
+        )
+   
+      expect(@user).to_not be_valid
+    end
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+    it "should login if email and password are good" do
+      @user = User.new(
+        first_name: "Test",
+        last_name: "Testington",
+        email: "test@test.com",
+        password: "heyo",
+        password_confirmation: "heyo"
+        )
+        @user.save
+
+        @session = User.authenticate_with_credentials("test@test.com", "heyo")
+
+      expect(@session).to_not be_nil
+    end
+ 
+  end
+
+end
+
